3b16a3b6cd7169c78b78ffbe1feb72f302eac00b
LinearPi <807745654@qq.com>
2019-02-22 18:25:05 +0800

    2-22

---
+++ Coding/ic/imageOCR.py
@@ -0,0 +1,32 @@
+# coding = utf-8
+import pytesseract
+import cv2
+import pyocr 
+from PIL import Image
+from PIL.ImageOps import invert
+import time
+
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
+image = Image.open('F:/PYcode/Coding/ic/mmm.jpg', mode='r').convert("L")
+
+# print(image.mode)
+# # print(type(image))
+# image = invert(image)
+# image= image.convert('1')
+
+# .convert("L")
+# 转灰色
+# convert("L")
+# 切片
+# crop (left, upper, right, lower)-tuple.
+# new_image = image.crop((0,68,500,310))
+
+# 保存灰色图片
+image.save(f'F:/PYcode/Coding/ic/{time.localtime().tm_hour}{time.localtime().tm_min}.png', quality=50)
+text = pytesseract.image_to_string(image, lang='chi_sim')
+
+# 保存文本信息
+with open('chi.txt','a',encoding='utf-8') as f:
+    f.write(text)
+
+print(text)  
