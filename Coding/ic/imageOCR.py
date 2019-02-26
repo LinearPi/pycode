4da9b0fc1503d3f45ae4f84d47db0f1589847f80
LinearPi <807745654@qq.com>
2019-02-26 18:14:37 +0800

    zhengqiangshuju

--- Coding/ic/imageOCR.py
+++ Coding/ic/imageOCR.py
@@ -7,9 +7,16 @@ from PIL.ImageOps import invert
 import time
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
-image = Image.open('F:/PYcode/Coding/ic/mmm.jpg', mode='r').convert("L")
+image = Image.open('F:/PYcode/Coding/ic/address.jpg', mode='r').convert("L")
+image.save(f'F:/PYcode/Coding/ic/{time.localtime().tm_hour}{time.localtime().tm_min}.png', quality=50)
+text = pytesseract.image_to_string(image, lang='chi_sim')
+print(text.replace(' ',''))  
+
+
 
 # print(image.mode)
+
+
 # # print(type(image))
 # image = invert(image)
 # image= image.convert('1')
@@ -22,11 +29,12 @@ image = Image.open('F:/PYcode/Coding/ic/mmm.jpg', mode='r').convert("L")
 # new_image = image.crop((0,68,500,310))
 
 # 保存灰色图片
-image.save(f'F:/PYcode/Coding/ic/{time.localtime().tm_hour}{time.localtime().tm_min}.png', quality=50)
-text = pytesseract.image_to_string(image, lang='chi_sim')
 
 # 保存文本信息
-with open('chi.txt','a',encoding='utf-8') as f:
-    f.write(text)
+# with open('chi.txt','a',encoding='utf-8') as f:
+    # f.write(text)
 
-print(text)  
+
+
+
+
--- Coding/ttt.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import torch 
-
-hello = tf.constant("hello world")
-sess = tf.Session()
-print(sess.run(hello))
-print(torch.__version__)
